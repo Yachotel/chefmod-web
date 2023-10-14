e2b76d8371e50d741306846515dbdfa0ff7c5fc9
anandureghu <anandukr145@gmail.com>
2023-10-14 22:42:39 +0530

    added sidebar, navbar, layout

---
+++ src/components/SIdebar/Sidebar.jsx
@@ -0,0 +1,11 @@
+import "./Sidebar.scss";
+
+const Sidebar = () => {
+  return (
+    <div className="sidebar">
+      <div className="sidebar__logo">Chefmode</div>
+    </div>
+  );
+};
+
+export default Sidebar;
--- src/main/pages/FallbackPage.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const FallbackPage = () => {
-  return <div>FallbackPage</div>;
-};
-
-export default FallbackPage;
